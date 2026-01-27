fd509751890cc6e529ee2a76ae92accdcb64c4a7
nefeshhaim770-maker <nefeshhaim770@gmail.com>
2026-01-27 16:53:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -192,7 +192,7 @@ async function createBankObligation(user, amount, note) {
         Comment1: note || "",
         FirstName: user.bankDetails.ownerName || user.name || "Donor",
         LastName: null,
-        ProjectNumber: "00001", // --- מעודכן: תואם לפורמט של האשראי (00001) ---
+        ProjectNumber: "31807", // --- מעודכן: שימוש במספר המסוף כמספר הפרויקט ---
         Mail: user.email || "no@mail.com",
         ReceiptName: user.receiptName || user.name || "",
         ReceiptFor: "",
@@ -248,7 +248,7 @@ async function createBankTransfer(user, amount, note) {
         Id: user.bankDetails.ownerID || user.tz, 
         TransferReason: note || "Salary Payment", 
         Currency: 1,
-        ProjectNumber: "00001", // --- מעודכן: תואם לפורמט של האשראי (00001) ---
+        ProjectNumber: "31807", // --- מעודכן: שימוש במספר המסוף כמספר הפרויקט ---
         
         // שדות אופציונליים
         Name: user.name || `${user.firstName} ${user.lastName}` || "", 
